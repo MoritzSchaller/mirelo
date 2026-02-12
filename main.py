c66e7cb6560c20d5720a2a28f4fda582d25e62cb
Moritz Schaller <kontakt@moritz-schaller.de>
2026-02-12 11:07:52 +0100

    Result formatting

--- main.py
+++ main.py
@@ -14,20 +14,24 @@ def main():
     
     print("Loading VGGSound dataset ...")
     df = get_vggsound_dataset(n_shards=1)
-    df = df.head()
+    # df = df.head()
+
     print("Classifying ...")
-    
     label_scores_df = classify_multiprocessed(df["path"])
-    print(label_scores_df)
     df = pd.concat((df, label_scores_df), axis=1)
-    print(df)
+    # print(df)
     
     print("Filtering ...")
-    speech_threshold = 0.5 # based on histogram
-    music_threshold = 0.5
-    df = df[(df["speech_score"] < speech_threshold) & (df["music_score"] < music_threshold)]
+    speech_threshold = 0.1 # based on histogram, exclude any possible speech
+    music_threshold = 0.1
 
-    print("Saving ...")
+    df = df[(df["speech_score"] < speech_threshold) & (df["music_score"] < music_threshold)]
+    # print(df)
+
+    print("Formatting and saving ...")
+    df = df.drop(columns=["YouTube ID", "start seconds", "train/test split", "path"])
+    df.columns = ["audio_text_description", "video_id", "speech_score", "music_score"]
+
     df.to_json("result_filtered.jsonl", orient="records", lines=True)
     
     print("Done!")
